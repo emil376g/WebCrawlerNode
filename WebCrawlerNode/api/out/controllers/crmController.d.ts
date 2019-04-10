de230f76c3ddf307a4ed7200c57033e4730269eb
Emil Giorgi <eg@truelinked.com>
2019-04-10 11:16:30 +0200

    fixing gitignore

--- WebCrawlerNode/api/out/controllers/crmController.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-export declare class ContactController {
-    addNewCrawl(req: Request, res: Response): Promise<void>;
-    getCrawl(req: Request, res: Response): Promise<void>;
-    getCrawlWithID(req: Request, res: Response): void;
-    updateCrawl(req: Request, res: Response): void;
-    deleteCrawl(req: Request, res: Response): void;
-}
